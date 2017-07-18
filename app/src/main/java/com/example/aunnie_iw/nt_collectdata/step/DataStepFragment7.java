4349d5a00f5ba2f9893bd3d3338ac243076f6dfc
Wasitthee <5610110493@psu.ac.th>
2017-07-18 14:45:00 +0700

    11/07/2017 : check internet

--- app/src/main/java/com/example/aunnie_iw/nt_collectdata/step/DataStepFragment7.java
+++ app/src/main/java/com/example/aunnie_iw/nt_collectdata/step/DataStepFragment7.java
@@ -5,8 +5,11 @@ package com.example.aunnie_iw.nt_collectdata.step;
  */
 
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.annotation.NonNull;
 import android.support.annotation.UiThread;
+import android.util.Log;
 
 import com.example.aunnie_iw.nt_collectdata.DataManager;
 import com.example.aunnie_iw.nt_collectdata.R;
@@ -68,5 +71,28 @@ public class DataStepFragment7 extends ButterKnifeFragment implements BlockingSt
     @Override
     protected int getLayoutResId() {
         return R.layout.fragment_step7;
+    }
+
+    public boolean haveNetworkConnection(){
+        boolean haveConnectedWifi = false;
+        boolean haveConnectedMobile = false;
+
+        ConnectivityManager cm = (ConnectivityManager)getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo[] networkInfos = cm.getAllNetworkInfo();
+        for (NetworkInfo networkInfo : networkInfos){
+            if (networkInfo.getTypeName().equalsIgnoreCase("WIFI")){
+                if (networkInfo.isConnected()){
+                    haveConnectedWifi = true;
+                    Log.d("haveWifi", String.valueOf(networkInfo.isConnected()));
+                }
+            }
+            if (networkInfo.getTypeName().equalsIgnoreCase("MOBILE")) {
+                if (networkInfo.isConnected()) {
+                    haveConnectedMobile = true;
+                    Log.d("haveMobile", String.valueOf(networkInfo.isConnected()));
+                }
+            }
+        }
+        return haveConnectedWifi||haveConnectedMobile;
     }
 }
